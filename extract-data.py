1cf4cc9257efbcb2ed07c4ec7cfe8b9e8a67a89e
Tiphaine Viard <tiphaine.viard@gmail.com>
2017-11-06 12:04:17 +0900

    Load data from data.txt in database

--- extract-data.py
+++ extract-data.py
@@ -1,51 +1,28 @@
 # coding:utf-8
-# 万 = \u4e07
-# 円 = \u5186
 
-# TODO : gérer quartier
-# TODO: Faire un pdf de courbes (matplotlib ?)
-# TODO : load from txt
+import sys
 from parsers import *
+import psycopg2
 
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import pyplot as plt
+try:
+	conn = psycopg2.connect(dbname="rooms-japan", host="localhost", user="tiphaine", password="tiphsolange")
+	cur = conn.cursor()
+except:
+	print("Unable to connect to database.")
+	sys.exit()
 
 # agharta = Agharta("https://www.realestate.co.jp/agharta/ja/rent/listing?page=1&prefecture=JP-13")
-# agharta = Agharta("https://apartments.gaijinpot.com/ja/rent/listing?prefecture=JP-13&max_price=150000&pets=1")
-# agharta.parse()
-# agharta.print("data-sample.txt")
+agharta = Agharta("https://apartments.gaijinpot.com/ja/rent/listing?prefecture=JP-13&max_price=150000&pets=1")
+# agharta.parse() -- should insert directly in db
 agharta.load("data.txt")
 
-# Make a plot ?
-# key1 = "surface"
-key1 = "surface"
-key2 = "rent"
-color = "walk_time"
-data_filter = [(x,y,c) for (x,y,c) in zip(agharta.get_key(key1), agharta.get_key(key2), agharta.get_key(color))] 
-x, y, c = zip(*data_filter)
-wards = list(set(c))
-# ipdb.set_trace()
-from sklearn.datasets import load_iris
-iris = load_iris()
+for p in agharta.props:
+	cur.execute("""
+		INSERT INTO dwellings (id, rent, admin_fee, surface, walk_time, location)
+		VALUES (DEFAULT, %s, %s, %s, %s, %s);""", (p["rent"], p["admin_fee"], p["surface"], p["walk_time"], p["ward"]))
+	print(p)
+# print(len(agharta.props))
 
-# use plt.cm.get_cmap(cmap, N) to get an N-bin version of cmap
-# plt.scatter(iris.data[:, 0], iris.data[:, 1], s=30,
-plt.scatter(x, y, c=[c.index(x) for x in c], cmap=plt.cm.get_cmap('ocean', len(wards)))
-
-# This function formatter will replace integers with target names
-formatter = plt.FuncFormatter(lambda val, loc: wards[val])
-
-# We must be sure to specify the ticks matching our target names
-plt.colorbar(ticks=[i for i in range(0, len(set(c)))], format=formatter);
-
-# Set the clim so that labels are centered on each block
-plt.clim(-0.5, 22.5)
-
-# plt.show()
-
-
-# plt.scatter(x, y, c=c, marker='o')
-plt.xlabel(key1)
-plt.ylabel(key2)
-# plt.colorbar(cmap=c)
-plt.savefig('surface-rent.eps', format='eps', dpi=300)
+# Insert into database.
+conn.commit()
+conn.close()
